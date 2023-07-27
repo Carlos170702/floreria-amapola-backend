b7cff6c4da85881374222f399918a4ac93c2288e
Carlos daniel <carloscruz1dev@gmail.com>
2023-07-26 22:51:01 -0600

    subir backend terminado

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ app.set("port", config.app.port);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // Permitir solicitudes desde cualquier origen
+  })
+);
 
 // routes
 app.use("/api/floreria", usuarios);
